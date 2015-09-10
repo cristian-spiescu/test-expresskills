5b678ebb6959a2f46380663fb808d4a4a48317db
cristian-spiescu <cristian.spiescu@crispico.com>
2015-09-10 14:43:18 +0300

    comit initial

---
+++ test_jvisualvm/src/test_jvisualvm/Main.java
@@ -0,0 +1,19 @@
+package test_jvisualvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		int counter = 0;
+		List<MyObject> list = new ArrayList<>();
+		while (true) {
+			list.add(new MyObject(counter++));
+			System.out.println(counter);
+			Thread.sleep(100);
+		}
+
+	}
+
+}
---
+++ test_jvisualvm/src/test_jvisualvm/MyObject.java
@@ -0,0 +1,19 @@
+package test_jvisualvm;
+
+public class MyObject {
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public MyObject(int id) {
+		super();
+		this.id = id;
+	}
+	
+}
