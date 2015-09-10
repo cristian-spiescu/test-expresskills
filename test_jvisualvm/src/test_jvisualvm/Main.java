87593e96d3558ebb6acc74a336417440fb1bb7fc
cristian-spiescu <cristian.spiescu@crispico.com>
2015-09-10 15:01:37 +0300

    3

--- test_jvisualvm/src/test_jvisualvm/Main.java
+++ test_jvisualvm/src/test_jvisualvm/Main.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
+		// inca un conflict E
 		// modificare din eclipse
 		int counter = 0;
 		// modificare non-conflictuala
