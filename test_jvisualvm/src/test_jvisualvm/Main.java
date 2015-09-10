ec55c2719bdbbca7c10fcde2c3d2c6e6f907abf9
cristian-spiescu <cristian.spiescu@crispico.com>
2015-09-10 14:52:49 +0300

    modificare eclipse

--- test_jvisualvm/src/test_jvisualvm/Main.java
+++ test_jvisualvm/src/test_jvisualvm/Main.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
+		// modificare din eclipse
 		int counter = 0;
 		List<MyObject> list = new ArrayList<>();
 		while (true) {
